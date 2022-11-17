38cff67dca7cdf8ba11404733e3b081940811574
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-11-17 20:00:45 +0530

    imported TodoForm & TodoList components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+const getTodoFromLocal = () => {
+	let list = localStorage.getItem("todos");
+	if (list) {
+		return JSON.parse(list);
+	} else {
+		return [];
+	}
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const initialValues = {
+		title: "",
+		desc: "",
+	};
+	const [todoInput, setTodoInput] = useState(initialValues);
+	const [todos, setTodos] = useState(getTodoFromLocal());
+	const [filter, setFilter] = useState("all");
+	const [filteredTodos, setFilteredTodos] = useState([]);
+	const [toggleBtn, setToggleBtn] = useState(false);
+	const [isEdit, setIsEdit] = useState(null);
+
+	useEffect(() => {
+		handleFilterTodo();
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [filter, todos]);
+
+	const handleFilterTodo = () => {
+		if (filter === "completed") {
+			setFilteredTodos(todos.filter((todo) => todo.completed === true));
+		} else if (filter === "pending") {
+			setFilteredTodos(todos.filter((todo) => todo.completed === false));
+		} else {
+			setFilteredTodos(todos);
+		}
+	};
+
+	return (
+		<div className="App">
+			<header>
+				<h1 className="todo-heading">Todos</h1>
+			</header>
+			<TodoForm
+				todoInput={todoInput}
+				setTodoInput={setTodoInput}
+				todos={todos}
+				setTodos={setTodos}
+				initialValues={initialValues}
+				setFilter={setFilter}
+				toggleBtn={toggleBtn}
+				setToggleBtn={setToggleBtn}
+				isEdit={isEdit}
+				setIsEdit={setIsEdit}
+			/>
+			<TodoList
+				todos={todos}
+				setTodos={setTodos}
+				setTodoInput={setTodoInput}
+				filteredTodos={filteredTodos}
+				setToggleBtn={setToggleBtn}
+				setIsEdit={setIsEdit}
+			/>
+		</div>
+	);
 }
 
 export default App;
