cde6e9cf91c9ea85a275b2ea6ba129d9f0c9645c
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-11-17 20:02:10 +0530

    edit todo functionality & filter functionality

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -7,6 +7,11 @@ const TodoForm = ({
 	todos,
 	setTodos,
 	initialValues,
+	setFilter,
+	toggleBtn,
+	setToggleBtn,
+	isEdit,
+	setIsEdit,
 }) => {
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
@@ -18,48 +23,75 @@ const TodoForm = ({
 
 	const addTodoHandler = (e) => {
 		e.preventDefault();
-		setTodos([
-			...todos,
-			{
-				title: todoInput.title,
-				desc: todoInput.desc,
-				completed: false,
-				id: uuidv4(),
-			},
-		]);
-		localStorage.setItem("todos", JSON.stringify(todos));
-		setTodoInput(initialValues);
+		if (!todoInput.title && !todoInput.desc) {
+			alert("Title and Description can't be empty");
+		} else if (todoInput.title && todoInput.desc && toggleBtn) {
+			setTodos(
+				todos.map((ele) => {
+					if (ele.id === isEdit) {
+						return { ...ele, title: todoInput.title, desc: todoInput.desc };
+					}
+					return ele;
+				})
+			);
+			setToggleBtn(false);
+			setTodoInput(initialValues);
+			setIsEdit(null);
+		} else {
+			setTodos([
+				...todos,
+				{
+					title: todoInput.title,
+					desc: todoInput.desc,
+					completed: false,
+					id: uuidv4(),
+				},
+			]);
+			setTodoInput(initialValues);
+		}
 	};
+
+	const handleFilter = (e) => {
+		setFilter(e.target.value);
+	};
+
 	return (
-		<div>
-			<form>
-				<input
-					type="text"
-					name="title"
-					placeholder="Enter Title"
-					value={todoInput.title}
-					onChange={handleInputChange}
-				/>
-				<textarea
-					rows="5"
-					type="text"
-					name="desc"
-					placeholder="Enter Description"
-					value={todoInput.desc}
-					onChange={handleInputChange}
-				/>
-				<div>
+		<form>
+			<input
+				type="text"
+				name="title"
+				placeholder="Enter Title"
+				className="todo-title"
+				value={todoInput.title}
+				onChange={handleInputChange}
+			/>
+			<textarea
+				rows="5"
+				type="text"
+				name="desc"
+				className="todo-desc"
+				placeholder="Enter Description"
+				value={todoInput.desc}
+				onChange={handleInputChange}
+			/>
+			<div className="btn-div">
+				{toggleBtn ? (
+					<button onClick={addTodoHandler} type="submit">
+						Update Todo
+					</button>
+				) : (
 					<button onClick={addTodoHandler} type="submit">
 						Add Todo
 					</button>
-					<select name="todos">
-						<option value="all">All</option>
-						<option value="pending">Pending</option>
-						<option value="completed">Completed</option>
-					</select>
-				</div>
-			</form>
-		</div>
+				)}
+
+				<select name="todos" onChange={handleFilter}>
+					<option value="all">All</option>
+					<option value="pending">Pending</option>
+					<option value="completed">Completed</option>
+				</select>
+			</div>
+		</form>
 	);
 };
 
